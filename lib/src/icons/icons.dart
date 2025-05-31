62b9a055ea6cbbea5730860cf645884103c729d5
Dimitris Papagiannakopoulos <dgpapagian@gmail.com>
2025-05-31 10:46:01 -0400

    Update icons.dart

--- lib/src/icons/icons.dart
+++ lib/src/icons/icons.dart
@@ -66,16 +66,15 @@ class _RestorePainter extends _IconPainter {
   void paint(Canvas canvas, Size size) {
     Paint p = getPaint(color);
     const double radius = 2.0;
-    final RRect backWindow = RRect.fromRectAndRadius(
-      Rect.fromLTRB(0, 2, size.width - 2, size.height),
-      Radius.circular(radius),
-    );
-
     final RRect frontWindow = RRect.fromRectAndRadius(
       Rect.fromLTRB(2, 0, size.width, size.height - 2),
       Radius.circular(radius),
     );
-
+    final RRect backWindow = RRect.fromRectAndRadius(
+      Rect.fromLTRB(0, 2, size.width - 2, size.height),
+      Radius.circular(radius),
+    );
+    canvas.drawRRect(frontWindow, p);
     final Path backPath = Path()..addRRect(backWindow);
     final Path frontPath = Path()..addRRect(frontWindow);
 
@@ -84,9 +83,7 @@ class _RestorePainter extends _IconPainter {
       backPath,
       frontPath,
     );
-
     canvas.drawPath(clippedBackPath, p);
-    canvas.drawRRect(frontWindow, p);
   }
 }
 
